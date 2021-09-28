a2c87c7e388e75e94680ade7c90c632476d404d6
AreYouWanNo <paramest.wsw@gmail.com>
2021-09-28 15:22:33 +0700

    bot music

--- bot.py
+++ bot.py
@@ -1,20 +1,42 @@
-# bot.py
-
+import discord,asyncio,youtube_dl
+from discord.ext import commands
 import os
-import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
 
-@client.event
-async def on_message(message):
-    message.content.lower()
-    if message.author == client.user:
-        return
-    if message.content.startswith('hello'):
-        await message.channel.send('Hi ' + str(message.author) +  ', Im Robot' )
 
-client.run(TOKEN)
+
+def get_prefix(bot, msg):
+    """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
+
+    prefixes = ['s.'] #Your bot prefix(s)
+
+    return commands.when_mentioned_or(*prefixes)(bot, msg)
+
+bot=commands.Bot(command_prefix=get_prefix,description='Multipurpose Discord Bot')
+
+
+
+
+exts=['music'] #Add your Cog extensions here
+
+
+@bot.event
+async def on_ready():
+    song_name='TWICE - What is love?'  #Status name
+    activity_type=discord.ActivityType.listening #Status type
+    await bot.change_presence(activity=discord.Activity(type=activity_type,name=song_name))
+    print(bot.user.name)
+
+
+
+
+
+
+for i in exts:
+    bot.load_extension(i)
+
+
+bot.run(os.environ['DISCORD_TOKEN'])
